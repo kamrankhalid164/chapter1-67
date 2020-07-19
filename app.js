ad7827a99f439c82801f416fd41ac49b5f9ff216
Kamran Ali <59841506+kamrankhalid164@users.noreply.github.com>
2020-07-20 04:04:38 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2060,6 +2060,91 @@ function nameValue() {
 // </body>
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//================================ chapter 53-58
+//Task1
+
+// <!DOCTYPE html>
+// <html lang="en">
+
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+//     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+//         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+// </head>
+// <style>
+//     body {
+//         text-align: center;
+//     }
+
+//     img {
+//         width: 100px;
+//         height: 100px;
+//     }
+//     #model{
+//         width: 500px;
+//         height: 300px;
+//     }
+// </style>
+
+// <body>
+//     <img src="1.jpg" alt="" data-toggle="modal" data-target="#exampleModal" onclick="displayModel(this)">
+//     <img src="2.jpg" alt="" data-toggle="modal" data-target="#exampleModal" onclick="displayModel(this)">
+//     <img src="3.jpg" alt="" data-toggle="modal" data-target="#exampleModal" onclick="displayModel(this)">
+//     <img src="4.png" alt="" data-toggle="modal" data-target="#exampleModal" onclick="displayModel(this)">
+    
+//     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+//         aria-hidden="true">
+//         <div class="modal-dialog">
+
+//         </div>
+//         <div>
+//             <img src="" alt="" id="model">
+//         </div>
+//     </div>
+
+
+// <script>
+//     function displayModel(perameter){
+//        var src=perameter.src
+//        modalImg=document.getElementById(model)
+//        model.src=src
+//     }
+// </script>
+//     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+//         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+//         crossorigin="anonymous"></script>
+//     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+//         integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+//         crossorigin="anonymous"></script>
+//     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+//         integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+//         crossorigin="anonymous"></script>
+// </body>
+// </html>
+
+
+
 //================================ chapter 58-67
 
 
